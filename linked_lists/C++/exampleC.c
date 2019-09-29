aa6647152dd2ab5f2b9234282b5d36b6ef419594
Jayaram Pemmanaboyidi <jayaram.pemmanaboyidi@gmail.com>
2019-09-29 16:47:15 +0530

    deleted one c++ folder

--- linked_lists/C++/exampleC.c
+++
@@ -1,8 +0,0 @@
-// C program to demonstrate difference 
-// in output in 32-bit and 64-bit gcc 
-// File name: geek.c 
-#include<stdio.h> 
-int main() 
-{ 
-    printf("Size = %lu", sizeof(size_t)); 
-}
